25bd54c27056c46f15f96cfdc1428682bedd2e9d
it-dreams <rahulraj.holi888@gmail.com>
2023-06-07 00:09:38 +0530

    Add test for react Element

---
+++ src/components/TestElement.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import TestElements from './TestElement';
+
+afterEach(cleanup);
+
+it('should equal to 0', () => {
+  const { getByTestId } = render(<TestElements />);
+  expect(getByTestId('counter')).toHaveTextContent(0);
+});
+ 
