de1de352cb9f51ef85afd1fbb5f4503799ec6b7a
it-dreams <rahulraj.holi888@gmail.com>
2023-06-07 01:06:24 +0530

    Added Tests for React Routers

---
+++ src/components/ReactRouter/Routers.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import TestRouter from './Routers';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(<Router history={history}>{component}</Router>),
+  };
+};
+
+it('should render the home page', () => {
+  const { container, getByTestId } = renderWithRouter(<TestRouter />);
+  const navbar = getByTestId('navbar');
+  const link = getByTestId('home-link');
+
+  expect(container.innerHTML).toMatch('Home page1');
+  expect(navbar).toContainElement(link);
+});
